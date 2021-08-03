7cfad56649ee98a96b89f1fb313831ce62524cfc
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-08-03 14:13:38 -0400

    Added a JavaScript and HTML file.

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+	var String = "Hey, thanks for obeying the command!";
+	var result = String.fontcolor("teal");
+	document.getElementById("Prompt").innerHTML =
+	result;
+}
